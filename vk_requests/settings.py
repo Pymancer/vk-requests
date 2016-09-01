be1ddbde2579c84655dd1d5feec01ff4f23285c5
pymancer <pymancer@gmail.com>
2016-09-01 21:52:49 +0300

    credentials read fix (for tox)

--- vk_requests/settings.py
+++ vk_requests/settings.py
@@ -4,7 +4,7 @@ import json
 import os.path
 
 # Set your environment variables for testing or put values here or into a credentials file
-creds_file = './creds.json'
+creds_file = './vk_requests/creds.json'
 creds = {"user_login": None, "user_password": None, "app_id": None, "phone_number": None}
 if os.path.isfile(creds_file):
     with open(creds_file) as f:
@@ -16,3 +16,4 @@ USER_PASSWORD = creds['user_password'] or os.getenv('VK_USER_PASSWORD', '')
 # aka API/Client ID
 APP_ID = creds['app_id'] or os.getenv('VK_APP_ID')
 PHONE_NUMBER = creds['phone_number'] or os.getenv('VK_PHONE_NUMBER')
+

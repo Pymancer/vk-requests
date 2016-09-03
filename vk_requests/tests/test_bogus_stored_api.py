0b112502d978b5e3c0214e1e7a0d274d059f661b
pymancer <pymancer@gmail.com>
2016-09-03 12:28:51 +0300

    empty stored token tests created

--- vk_requests/tests/test_bogus_stored_api.py
+++ vk_requests/tests/test_bogus_stored_api.py
@@ -119,3 +119,115 @@ class VkTestCase(unittest.TestCase):
         self.assertIn('items', resp)
         for user_id in resp['items']:
             self.assertIsInstance(user_id, int)
+
+
+class VkEmptyTokenTestCase(unittest.TestCase):
+    STORED_TOKEN = ''
+
+    def setUp(self):
+        self.vk_api = vk_requests.create_api(lang='ru')
+
+    @staticmethod
+    def _create_api(**kwargs):
+        return vk_requests.create_api(
+            app_id=APP_ID,
+            login=USER_LOGIN,
+            password=USER_PASSWORD,
+            stored_token = VkTestCase.STORED_TOKEN,
+            session_cls=StoredVKSession,
+            **kwargs
+        )
+
+    def test_get_server_time(self):
+        time_1 = time.time() - 5
+        time_2 = time_1 + 10
+        server_time = self.vk_api.getServerTime()
+        self.assertTrue(time_1 <= server_time <= time_2)
+
+    def test_get_server_time_via_token_api(self):
+        time_1 = time.time() - 5
+        time_2 = time_1 + 20
+        server_time = self._create_api().getServerTime()
+        self.assertTrue(time_1 <= server_time <= time_2)
+
+    def test_get_profiles_via_token(self):
+        profiles = self.vk_api.users.get(user_id=1)
+        self.assertEqual(profiles[0]['last_name'], u'Дуров')
+
+    def test_users_search(self):
+        request_opts = dict(
+            city=2,
+            age_from=18,
+            age_to=50,
+            offset=0,
+            count=1000,
+            fields=['screen_name'])
+
+        # Expect api error because search method requires access token
+        with self.assertRaises(VkAPIError) as err:
+            resp = self.vk_api.users.search(**request_opts)
+            self.assertIsNone(resp)
+            self.assertIn('no access_token passed', str(err))
+
+        # Create token-based API
+        api = self._create_api()
+        resp = api.users.search(**request_opts)
+        self.assertIsInstance(resp, dict)
+        total_num, items = resp['count'], resp['items']
+        self.assertIsInstance(total_num, int)
+        for item in items:
+            self.assertIsInstance(item, dict)
+            self.assertIn('screen_name', item)
+
+    def test_get_friends(self):
+        items = self.vk_api.friends.get(
+            fields=['nickname', 'city', 'can_see_all_posts'],
+            user_id=1)
+        self.assertIsInstance(items, dict)
+        friends = items['items']
+        for item in friends:
+            if 'deactivated' in item:
+                # skip deactivated users, they don't have extra fields
+                continue
+            self.assertIsInstance(item, dict)
+
+            # User can hide this field
+            # self.assertIn('city', item)
+            self.assertIn('nickname', item)
+            self.assertIn('id', item)
+            self.assertIn('can_see_all_posts', item)
+
+    def test_set_status(self):
+        """Test requires scope='status' vk permissions
+        """
+        status_text = 'Welcome to noagent.168.estate'
+        api = self._create_api(scope=['offline', 'status'])
+        self.assertEqual(api._session.auth_api.scope, ['offline', 'status'])
+
+        # Set the status
+        resp = api.status.set(text=status_text)
+        self.assertEqual(resp, 1)
+
+        # Check the status
+        resp = api.status.get()
+        self.assertEqual(resp, {'text': status_text})
+
+    def test_multi_scope_requests(self):
+        api = self._create_api(scope=['messages', 'status'])
+        resp = api.status.get()
+        self.assertIn('text', resp)
+
+        resp = api.messages.get()
+        self.assertIsInstance(resp, dict)
+        total_msg, msg_list = resp['count'], resp['items']
+        self.assertIsInstance(total_msg, int)
+        for msg in msg_list:
+            self.assertIsInstance(msg, dict)
+
+    def test_likes_get_list(self):
+        api = self._create_api()
+        resp = api.likes.getList(type='post', owner_id=1, item_id=815649)
+        self.assertIn('count', resp)
+        self.assertIn('items', resp)
+        for user_id in resp['items']:
+            self.assertIsInstance(user_id, int)
--- vk_requests/tests/test_empty_bogus_stored_api.py
+++ vk_requests/tests/test_empty_bogus_stored_api.py
@@ -72,3 +72,66 @@ class VkTestCase(unittest.TestCase):
             self.assertIn('id', item)
             self.assertIn('can_see_all_posts', item)
 
+
+class VkEmptyTokenTestCase(unittest.TestCase):
+    STORED_TOKEN = ''
+
+    def setUp(self):
+        self.vk_api = vk_requests.create_api(lang='ru')
+
+    @staticmethod
+    def _create_api(**kwargs):
+        return vk_requests.create_api(
+            stored_token = VkTestCase.STORED_TOKEN,
+            session_cls=StoredVKSession,
+            **kwargs
+        )
+
+    def test_get_server_time(self):
+        time_1 = time.time() - 5
+        time_2 = time_1 + 30
+        server_time = self.vk_api.getServerTime()
+        self.assertTrue(time_1 <= server_time <= time_2)
+
+    def test_get_server_time_via_token_api(self):
+        with self.assertRaises(ValueError) as err:
+            resp = self._create_api().getServerTime()
+            self.assertIsNone(resp)
+            self.assertIn('must be given', str(err))
+
+    def test_get_profiles_via_token(self):
+        profiles = self.vk_api.users.get(user_id=1)
+        self.assertEqual(profiles[0]['last_name'], u'Дуров')
+
+    def test_users_search(self):
+        request_opts = dict(
+            city=2,
+            age_from=18,
+            age_to=50,
+            offset=0,
+            count=1000,
+            fields=['screen_name'])
+
+        # Expect api error because search method requires access token
+        with self.assertRaises(VkAPIError) as err:
+            resp = self.vk_api.users.search(**request_opts)
+            self.assertIsNone(resp)
+            self.assertIn('no access_token passed', str(err))
+
+    def test_get_friends(self):
+        items = self.vk_api.friends.get(
+            fields=['nickname', 'city', 'can_see_all_posts'],
+            user_id=1)
+        self.assertIsInstance(items, dict)
+        friends = items['items']
+        for item in friends:
+            if 'deactivated' in item:
+                # skip deactivated users, they don't have extra fields
+                continue
+            self.assertIsInstance(item, dict)
+
+            # User can hide this field
+            # self.assertIn('city', item)
+            self.assertIn('nickname', item)
+            self.assertIn('id', item)
+            self.assertIn('can_see_all_posts', item)
